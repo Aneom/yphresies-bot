b9c3535eeb53c649bc7cace50b923aa0b0d5d1f2
Antonis K <antoniskal001@gmail.com>
2025-09-30 14:50:33 +0300

    cosmetic changes

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ full_names = stavroi.index.values
 unicode_surname_dict = {unidecode(sldr.split()[1]): sldr for sldr in full_names}
 
 # Duties dictionary creation (to match codenames with explanation)
-# Here we execute the following function in order to get a dictionary that helps match codenames in Excel with user-interpretable output.
-# Because the DUTIES_DICT is a dictionary which is generated using dictionary comprehensions, it has to come from a function and not just an env var.
+# For GitHub reviewers: This function is not included as it reveals sensitive information
 import get_duties
 DUTIES_DICT = get_duties.get_duties_dict()
 
@@ -40,7 +39,7 @@ def map_name_input(surname: str, mapping_dict: dict) -> str:
     return soldier_full_name
 
 
-def scan_duties_v3(soldier_full_name: str, stavroi_excel: pd.DataFrame, duties_dict: dict, days_dict: dict) -> None:
+def scan_duties(soldier_full_name: str, stavroi_excel: pd.DataFrame, duties_dict: dict, days_dict: dict) -> None:
     # Calendar stuff to get month, year and the amount of days in the current month
     cur_month = datetime.now().month
     cur_year = datetime.now().year
@@ -79,77 +78,6 @@ def scan_duties_v3(soldier_full_name: str, stavroi_excel: pd.DataFrame, duties_d
     return
 
 
-def scan_duties_v1(soldier_full_name: str, stavroi_excel: pd.DataFrame, duties_dict: dict, days_dict: dict) -> None:
-    # !UNUSED FUNCTION!
-    # Calendar stuff to get month, year and the amount of days in the current month
-    cur_month = datetime.now().month
-    cur_year = datetime.now().year
-    _, month_range = monthrange(year=cur_year, month=cur_month)
-
-    curr_day = datetime.today().day
-    # We get a pd.Series with just the duties per day for simplicity reasons
-    duties = stavroi_excel.loc[soldier_full_name][3:3+month_range]
-
-    curr_day = datetime.today().day
-    day_counter = 1  # first day of the month
-    for duty in duties:
-        if day_counter < curr_day:
-            day_counter +=1
-        else:
-            duty = duty.upper().strip()
-            if duty != 'Ε':  # if duty not in ('Ε', 'ΚΑ'):
-                day_name = days_dict[weekday(cur_year, cur_month, day_counter)]
-                try:
-                    if duty == 'ΤΙΜ':
-                        print(f'Την ημέρα {day_name} {day_counter}/{cur_month}/{cur_year} είσαι αδειούχος🎉')
-                    elif duty == 'ΟΡΓ':
-                        print(f'Την ημέρα {day_name} {day_counter}/{cur_month}/{cur_year} είσαι Όργανο')
-                    elif duty == 'ΒΑΥΔΜ':
-                        print(f'Την ημέρα {day_name} {day_counter}/{cur_month}/{cur_year} είσαι ΒΑΥΔΜ')
-                    elif duty == 'ΚΑ':
-                        print(f'Την ημέρα {day_name} {day_counter}/{cur_month}/{cur_year} έχεις ζητήσει κάτι')
-                    else:
-                        print(f'Την ημέρα {day_name} {day_counter}/{cur_month}/{cur_year} έχεις υπηρεσία: {duties_dict[duty]}')
-                except KeyError:
-                    # print(f'ΣΦΑΛΜΑ: ο κωδικός "{duty}", δεν αντιστοιχεί σε όνομα υπηρεσίας ({day_counter}/{cur_month}/{cur_year})')
-                    pass
-            day_counter +=1  # we move on to the next day
-    return
-
-
-def scan_duties2(soldier_full_name: str, stavroi_excel: pd.DataFrame, duties_dict: dict, days_dict: dict) -> None:
-    # !UNUSED FUNCTION!
-    # Calendar stuff to get month, year and the amount of days in the current month
-    cur_month = datetime.now().month
-    cur_year = datetime.now().year
-    _, month_range = monthrange(year=cur_year, month=cur_month)
-
-    # We get a pd.Series with just the duties per day for simplicity reasons
-    duties = stavroi_excel.loc[soldier_full_name][3:3+month_range]
-
-    day = datetime.today().day  # first day of the month
-    for day in range(day, month_range+1):
-        duty = duties.iat[day-1].upper().strip()
-        if duty != 'Ε':  # if duty not in ('Ε', 'ΚΑ'):
-            day_name = days_dict[weekday(cur_year, cur_month, day)]
-            try:
-                if duty == 'ΤΙΜ':
-                    print(f'Την ημέρα {day_name} {day}/{cur_month}/{cur_year} είσαι αδειούχος🎉')
-                elif duty == 'ΟΡΓ':
-                    print(f'Την ημέρα {day_name} {day}/{cur_month}/{cur_year} είσαι Όργανο')
-                elif duty == 'ΒΑΥΔΜ':
-                    print(f'Την ημέρα {day_name} {day}/{cur_month}/{cur_year} είσαι ΒΑΥΔΜ')
-                elif duty == 'ΚΑ':
-                    print(f'Την ημέρα {day_name} {day}/{cur_month}/{cur_year} μας έχεις ζητήσει κάτι')
-                else:
-                    print(f'Την ημέρα {day_name} {day}/{cur_month}/{cur_year} έχεις υπηρεσία: {duties_dict[duty]}')
-            except KeyError:
-                # print(f'ΣΦΑΛΜΑ: ο κωδικός "{duty}", δεν αντιστοιχεί σε όνομα υπηρεσίας ({day_counter}/{cur_month}/{cur_year})')
-                pass
-        day +=1  # we move on to the next day
-    return
-
-
 if __name__ == "__main__":
     soldier_surname = input('Αλτ, τις ει;\n(επώνυμο): ')
 
@@ -159,7 +87,7 @@ if __name__ == "__main__":
         print(f'Σφάλμα: Ο στρατιώτης με επώνυμο "{soldier_surname}" δε βρέθηκε.')
         # print(f"Σφάλμα: Το όνομα σου δε βρέθηκε στο Excel.\nΒεβαιώσου ότι το username σου στο Discord είναι σωστό και της μορφής '[Επώνυμο]<ΚΕΝΟ>[κινητό τηλέφωνο]' (πχ Παντόφλας 6912345678).")
     else:
-        scan_duties_v3(soldier_full_name=soldier_full_name,
+        scan_duties(soldier_full_name=soldier_full_name,
                     stavroi_excel=stavroi,
                     duties_dict=DUTIES_DICT,
                     days_dict=DAYS_DICT)
